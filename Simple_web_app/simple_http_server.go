ed51b68933d3760f3278a7f7be516d743ed3dbc7
MaxOvcharov <ovhcrovmax@yandex.ru>
2019-04-05 23:03:02 +0300

    MO - add simple view

--- Simple_web_app/simple_http_server.go
+++ Simple_web_app/simple_http_server.go
@@ -1,17 +1,38 @@
 package main
 
-
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	return &Page{Title: title, Body: []byte("This is a sample Page.")}, nil
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
