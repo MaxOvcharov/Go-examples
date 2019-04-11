f2e58e215abeeb77b0ff8c657c03a9878a08a743
MaxOvcharov <ovhcrovmax@yandex.ru>
2019-04-11 08:53:53 +0300

    MO - add editHandler

--- Simple_web_app/simple_http_server.go
+++ Simple_web_app/simple_http_server.go
@@ -18,7 +18,14 @@ func (p *Page) save() error {
 }
 
 func loadPage(title string) (*Page, error) {
-	return &Page{Title: title, Body: []byte("This is a sample Page.")}, nil
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: title, Body: body}, nil
 }
 
 func simpleHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,8 +38,25 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
 	http.HandleFunc("/", simpleHandler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
