8de9af20edc058418b890944feb053825b9577e6
MaxOvcharov <ovhcrovmax@yandex.ru>
2019-05-30 22:59:46 +0300

    MO - add simple client for JWT token generation

---
+++ simple_jwt_example/client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var mySigningString = []byte("mysupersecretkey")
+
+
+func GenerateJWT()(string, error){
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["autorized"] = true
+	claims["user"] = "Max Ovcharov"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySigningString)
+
+	if err != nil {
+		fmt.Errorf("something went wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func main() {
+	tokenString, err := GenerateJWT()
+
+	if err != nil {
+		fmt.Println("Error generating token string")
+	}
+
+	fmt.Println(tokenString)
+}
